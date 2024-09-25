36b18e1564d36db4ec824afbf05775870680f83d
0xurb <urbadeil@gmail.com>
2024-09-25 20:22:19 +0100

    chore: cleanup backfill

--- reth-src/exex/crates/backfill/src/lib.rs
+++
@@ -1,5 +0,0 @@
-#![cfg_attr(not(test), warn(unused_crate_dependencies))]
-#![allow(missing_docs)]
-
-mod message;
-pub use message::*;
--- reth-src/exex/crates/exex/src/plugin.rs
+++ reth-src/exex/crates/exex/src/plugin.rs
@@ -6,6 +6,11 @@ use reth::api::FullNodeComponents;
 use reth_exex::ExExContext;
 use reth_tracing::tracing::info;
 
+/// This interface is used to simplify `ExEx` logic and split it into:
+/// - `setup` - some routine before `ExEx` is launched. Can be async, can be ignored (has default
+///   implementation).
+/// - `exex` - main logic of `ExEx` with [`ExExContext`] as a given param and [`ExExNotification`]
+///   handler from this context notification channel.
 pub trait ExExPlugin<Id>: Sized
 where
     Id: Into<String> + Debug,
@@ -14,13 +19,15 @@ where
 
     /// `ExEx` setup routine, if needed
     /// Can be a DB connection pool opening, state syncing, etc.
-    fn setup(&self) -> impl Future<Output = Result<()>> + Send;
+    fn setup(&self) -> impl Future<Output = Result<()>> + Send {
+        async move { Ok(()) }
+    }
 
     /// The `ExEx` future.
     /// Implements a logic to:
-    /// - receive context notifications (`ExExNotification`)
-    /// - process chain of blocks and their final state (`Chain`)
-    /// - send events (`ExExEvent`) to context event channel
+    /// - receive context notifications ([ExExNotification](`reth_exex::ExExNotification`))
+    /// - process chain of blocks and their final state ([chain](`Chain`))
+    /// - send events [ExExEvent](`reth_exex::ExExEvent`) to context event channel
     fn exex<N: FullNodeComponents>(
         self,
         ctx: ExExContext<N>,
